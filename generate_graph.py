bb82d9f37710afd683251eb0e7a59186db17ded0
Kshitij Kapoor <k00lk0der1@users.noreply.github.com>
2025-10-19 16:57:30 +0100

    Add args

--- generate_graph.py
+++ generate_graph.py
@@ -3,10 +3,10 @@ import argparse
 
 parser = argparse.ArgumentParser("PAN Random Graph Generator")
 
-parser.add_argument("alpha", help="alpha parameter.", type=float)
-parser.add_argument("beta", help="beta parameter.", type=float)
-parser.add_argument("n_nodes", help="Number of nodes in each graph.", type=int)
-parser.add_argument("n_samples", help="Number of graphs to generate.", type=int)
+parser.add_argument("--alpha", help="alpha parameter.", type=float)
+parser.add_argument("--beta", help="beta parameter.", type=float)
+parser.add_argument("--n_nodes", help="Number of nodes in each graph.", type=int)
+parser.add_argument("--n_samples", help="Number of graphs to generate.", type=int)
 
 args = parser.parse_args()
 
