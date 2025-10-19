25dd19025ed185eaa0501ece4bf28592bc564284
Kshitij Kapoor <k00lk0der1@users.noreply.github.com>
2025-10-19 16:59:30 +0100

    Add args

--- generate_graph.py
+++ generate_graph.py
@@ -7,6 +7,7 @@ parser.add_argument("--alpha", help="alpha parameter.", type=float)
 parser.add_argument("--beta", help="beta parameter.", type=float)
 parser.add_argument("--n_nodes", help="Number of nodes in each graph.", type=int)
 parser.add_argument("--n_samples", help="Number of graphs to generate.", type=int)
+parser.add_argument("--output_directory", help="Directory to save graphs in.", type=str)
 
 args = parser.parse_args()
 
