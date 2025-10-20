00e83499c875ce418e7b84830f01f2fdcf915000
Kshitij Kapoor <k00lk0der1@users.noreply.github.com>
2025-10-20 12:31:12 +0100

    Partially complete sampling

--- package/graphs/preferential_attachment_network.py
+++ package/graphs/preferential_attachment_network.py
@@ -1,17 +1,18 @@
+import copy
 import numpy as np
 
-import jax
-import jax.numpy as jnp
+#import jax
+#import jax.numpy as jnp
 
-import pytensor.tensor as pt 
-import pymc as pm
+#import pytensor.tensor as pt 
+#import pymc as pm
 
 
 class PreferentialAttachmentNetwork:
     def __init__(self):
         self.observed = False
     
-    def generate(self, alpha, beta, n_nodes, random_state_seed=None):
+    def generate_sample(self, alpha, beta, n_nodes, random_state_seed=None):
         
         #Setting the seed and generating a RandomState object.
         self.random_state_seed = random_state_seed
@@ -36,13 +37,33 @@ class PreferentialAttachmentNetwork:
                 parent_node_probability_propto.sum()
             )
 
-            
+            chosen_node_index = (parent_node_probability_cdf<uniform_samples[t-2]).argmax()
 
-            
-
-
-
+            self.d_t[t] = self.degrees[chosen_node_index]
+            self.degrees[t] = 1
+            self.degrees[chosen_node_index] = self.degrees[chosen_node_index] + 1
         
+        N_dict = {
+            2:{1:2}
+        }
+
+        for t in range(3, n_nodes):
+            N_dict[t] = copy.deepcopy(N_dict[t-1])
+            N_dict[t][1] = N_dict[t][1] + 1
+            N_dict[t][self.d_t[t-1]] = N_dict[t][self.d_t[t-1]] - 1
+            if self.d_t[t-1]+1 in N_dict[t].keys():
+                N_dict[t][self.d_t[t-1]+1] = N_dict[t][self.d_t[t-1]] - 1
+            else:
+                N_dict[t][self.d_t[t-1]+1] = 1
+            
+            if(N_dict[t][self.d_t[t-1]]==0):
+                N_dict[t].pop(self.d_t[t-1]])
+            
+            print(N_dict)
+            
+        
+        self.observed = True
+                    
 
     def negative_log_likelihood(self, alpha, beta, n_nodes):
         pass
