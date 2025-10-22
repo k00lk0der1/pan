c18fd939c277523313ba67dff9579297bf49d802
Kshitij Kapoor <k00lk0der1@users.noreply.github.com>
2025-10-22 16:36:40 +0100

    adding mp to mle_dist

--- mle_dist.py
+++ mle_dist.py
@@ -11,13 +11,14 @@ parser.add_argument("--beta", help="beta parameter.", type=float)
 parser.add_argument("--n_nodes", help="Number of nodes in each graph.", type=int)
 parser.add_argument("--output_directory", help="Directory to save graphs in.", type=str)
 parser.add_argument("--n_samples_max", help="Maximum number of samples to use", type=int)
+parser.add_argument("--n_processes", help="Number of processes/cores to use to generate graphs", type=int)
 
 
 args = parser.parse_args()
 filenames = os.listdir(args.output_directory)
 
 filtered_files = [
-    filename for filename in filenames if (
+    os.path.join(args.output_directory, filename) for filename in filenames if (
         f"alpha={args.alpha}" in filename and
         f"beta={args.beta}" in filename and
         f"nodes={args.n_nodes}" in filename
@@ -28,19 +29,17 @@ n = len(filtered_files)
 
 print("Number of Samples :", n)
 
-mles = np.array(
-    [
-        dill.load(
-            open(
-                os.path.join(
-                    args.output_directory,
-                    filename
-                ),
-                "rb"
-            )
-        ).numerical_mle().x for filename in filtered_files
-    ]
-)
+def numerical_mle(pan_obj_filename):
+    return dill.load(open(pan_obj_filename, "rb")).numerical_mle().x
+
+mp_pool = multiprocess.Pool(processes=args.n_processes)
+
+pan_objs = mp_pool.starmap(numerical_mle, filtered_files)
+
+mp_pool.close()
+mp_pool.join()
+
+mles = np.array(mp_pool.map(numerical_mle, filtered_files))
 
 print(mles.shape)
 
